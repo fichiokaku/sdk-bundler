b0e116ca6970d6f7f89fb5961dc8ad90b98212c9
fichiokaku <filip@polyflow.dev>
2023-04-19 02:14:40 +0200

    update sdk

--- scripts/injector-template.js
+++ scripts/injector-template.js
@@ -1,4 +1,9 @@
-import { attach } from "@polyflow/sdk";
+import { attach, initializeWsProxy } from "@polyflow/sdk";
+
+window.addEventListener("DOMContentLoaded", () => {
+    initializeWsProxy();
+});
+
 window.addEventListener("load", () => {
     attach("API_KEY");
 });
